fc45453661a466bfea1d2e6a0cdc1ae4a2a96df3
Suji-12 <23csec23sujithas@gmail.com>
2025-09-18 18:17:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
-//your code here
-<script type="text/javascript" src="./script.js">
-	const div = document.createElement("div");
-    const text = document.createTextNode("My current favourite song is ");
-    //Create the anchor element
-    const link = document.createElement("a");
-    link.href = "http://youtube.com/watch?v=OXHTlMPbX7o&list=RDOXHTlMPbX7o&start_radio=1"; //YouTube link
-    link.textContent = "Powerhouse"; //song name
-    div.appendChild(text);
-    div.appendChild(link);
-    document.body.appendChild(div);
-</script>
+// Song details
+const songName = "Power house";
+const youtubeLink = "https://www.youtube.com/watch?v=OXHTlMPbX7o&list=RDOXHTlMPbX7o&start_radio=1";
+
+// Get the existing div by id
+const divTag = document.getElementById("song-container");
+
+// Insert the text + link inside the div
+divTag.innerHTML = `My current favourite song is 
+  <a href="${youtubeLink}" target="_blank">${songName}</a>`;
